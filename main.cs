87d01f684262874a02e19bf294c5b458d5213409
SimonB2009 <brandstetter.simon@web.de>
2023-05-07 19:51:23 +0200

    added MX logo and instruction with first menue

--- main.cs
+++ main.cs
@@ -6,14 +6,53 @@ namespace MissionXMenue
    class Menue : Run  {
 
       static void Main(string[] args) {
-         //Run.run();
          clear();
+         showInstruction();
+         showMenue();
       }
 
-      static void clear() {
+      static void clear() { // first clear Console
          for (int i = 0; i < 100; i++) {
             Console.WriteLine();
          }
+      }
+
+      static void showInstruction() { // Instruction and Name
+         showLogo();
+         Console.WriteLine("");
+         Console.WriteLine("");
+         Console.ForegroundColor = ConsoleColor.White;
+         Console.Write("This Game is named "); Console.ForegroundColor = ConsoleColor.Green; Console.Write("MissionX ");
+         Console.ForegroundColor = ConsoleColor.White; Console.WriteLine("and the goal is to land the rocket which is in air."); 
+         Console.WriteLine("You have only a bit fuel to land it, so save your fuel. Good luck!");
+         Console.WriteLine("");
+         Console.WriteLine("");
+         Console.WriteLine("");
+         Console.WriteLine("");
+      }
+
+      static void showLogo() {
+         Console.ForegroundColor = ConsoleColor.Green;
+         Console.WriteLine("               ++++          ++++     ++    ++");
+         Console.WriteLine("               ++ ++        ++ ++      ++  ++");
+         Console.WriteLine("               ++  ++      ++  ++       ++++");
+         Console.WriteLine("               ++   ++    ++   ++       ++++");
+         Console.WriteLine("               ++    ++  ++    ++      ++  ++");
+         Console.WriteLine("               ++     ++++     ++     ++    ++");
+         
+
+      }
+
+      static void showMenue() {
+         Console.ForegroundColor = ConsoleColor.DarkYellow;
+         Console.WriteLine("                             MENUE");
+         Console.WriteLine("");
+         Console.WriteLine("");
+         Console.ForegroundColor = ConsoleColor.White;
+         Console.WriteLine("-This Game is named MissionX and the goal is to land the rocket which is in air."); 
+         Console.WriteLine("-You have only a bit fuel to land it, so save your fuel. Good luck!");
+         Console.WriteLine("");
+         Console.WriteLine("");
       }
    }
 }
