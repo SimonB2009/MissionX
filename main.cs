17eb9d46943b5df814f788a534709f11b9d45019
SimonB2009 <brandstetter.simon@web.de>
2023-05-07 20:06:53 +0200

    added nice menue and the m command

--- main.cs
+++ main.cs
@@ -9,6 +9,17 @@ namespace MissionXMenue
          clear();
          showInstruction();
          showMenue();
+
+         Console.ForegroundColor = ConsoleColor.Magenta;
+         Console.Write("⟫ ");
+
+         do {
+            if (Console.ReadLine() == "m") {
+               Console.Write("");
+               Console.Write("");
+               showMenue();
+            }
+         } while (true);     
       }
 
       static void clear() { // first clear Console
@@ -45,12 +56,14 @@ namespace MissionXMenue
 
       static void showMenue() {
          Console.ForegroundColor = ConsoleColor.DarkYellow;
-         Console.WriteLine("                             MENUE");
+         Console.WriteLine("               MENUE");
          Console.WriteLine("");
          Console.WriteLine("");
          Console.ForegroundColor = ConsoleColor.White;
-         Console.WriteLine("-This Game is named MissionX and the goal is to land the rocket which is in air."); 
-         Console.WriteLine("-You have only a bit fuel to land it, so save your fuel. Good luck!");
+         Console.WriteLine("- Press \"s\" to start"); 
+         Console.WriteLine("- Press \"l\" to leave the game");
+         Console.WriteLine("- Press \"i\" to get the instruction for the game.");
+         Console.WriteLine("- Press \"m\" to go back to the menue");
          Console.WriteLine("");
          Console.WriteLine("");
       }
